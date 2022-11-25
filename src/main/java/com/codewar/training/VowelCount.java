519ef7e7c68adb5978b7ed23de49d71bdf23696a
Wallace Winner <wallacewinner@hotmail.com>
2022-11-25 00:41:20 -0300

    fix: assertEquals on testCase1

--- src/main/java/com/codewar/training/VowelCount.java
+++ src/main/java/com/codewar/training/VowelCount.java
@@ -1,7 +1,16 @@
 package com.codewar.training;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class VowelCount {
     public static int getCount(String str){
-        return 0;
+        Pattern pattern = Pattern.compile("[aeiou]", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(str);
+        int count = 0;
+        while (matcher.find()){
+            count++;
+        }
+        return count;
     }
 }
