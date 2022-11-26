8ab5f67ad099fd6d99af59450c22a33f4f9b9415
Wallace Winner <wallacewinner@hotmail.com>
2022-11-26 16:14:35 -0300

    fix: set a performance method to reverse a text

--- src/main/java/com/codewar/training/ReversedWords.java
+++ src/main/java/com/codewar/training/ReversedWords.java
@@ -1,14 +1,14 @@
 package com.codewar.training;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class ReversedWords {
     public static String reverseWords(String str){
-        String[] stringArray = str.split(" ");
-        int stringArrayLength = stringArray.length;
-        StringBuilder textreversed = new StringBuilder();
-        while (stringArrayLength > 0){
-            textreversed.append(stringArray[stringArrayLength - 1 ]).append(" ");
-            stringArrayLength --;
-        }
-        return textreversed.toString().trim();
+        List words = Arrays.asList(str.split(" "));
+        Collections.reverse(words);
+
+        return String.join(" ",words);
     }
 }
