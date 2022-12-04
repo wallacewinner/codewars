04a867973cae288f7ab9c9e0e38a718813ddcd14
Wallace Winner <wallacewinner@hotmail.com>
2022-12-04 16:16:34 -0300

    feat: set solution on InvertValues

--- src/main/java/com/codewar/training/InvertValues.java
+++ src/main/java/com/codewar/training/InvertValues.java
@@ -8,6 +8,8 @@ package com.codewar.training;
  */
 public class InvertValues {
     public static int[] invert(int[] array) {
-        return null;
+        for (int i = 0; i <= array.length - 1; i++)
+           array[i] = array[i] * -1;
+        return array;
     }
 }
