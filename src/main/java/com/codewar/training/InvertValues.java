d54c55b074bbd55483b4edac3cdc95c3f1ea77c3
Wallace Winner <wallacewinner@hotmail.com>
2022-12-04 16:00:44 -0300

    doc: add instructions

---
+++ src/main/java/com/codewar/training/InvertValues.java
@@ -0,0 +1,13 @@
+package com.codewar.training;
+
+/**
+ * Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+ * invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+ * invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+ * invert([]) == []
+ */
+public class InvertValues {
+    public static int[] invert(int[] array) {
+        return null;
+    }
+}
