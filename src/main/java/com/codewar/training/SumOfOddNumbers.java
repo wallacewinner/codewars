b0faede8ba038af3cbe9250389e2e51984a7630b
Wallace Winner <wallacewinner@hotmail.com>
2022-12-20 20:59:58 -0300

    feat: set solution to sum of odd numbers

--- src/main/java/com/codewar/training/SumOfOddNumbers.java
+++ src/main/java/com/codewar/training/SumOfOddNumbers.java
@@ -1,7 +1,19 @@
 package com.codewar.training;
 
+/**
+ * Given the triangle of consecutive odd numbers:
+ *
+ *              1
+ *           3     5
+ *        7     9    11
+ *    13    15    17    19
+ * 21    23    25    27    29
+ * Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+ * 1 -->  1
+ * 2 --> 3 + 5 = 8
+ */
 public class SumOfOddNumbers {
     public static int rowSumOddNumbers(int  n) {
-        return -1;
+        return n * n * n;
     };
 }
