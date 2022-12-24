6e4e6783df0949f24d57d6943e0ccff5d8cadcac
Wallace Winner <wallacewinner@hotmail.com>
2022-12-24 17:17:20 -0300

    doc: set instructions

---
+++ src/test/java/com/codewar/training/IsogramsTest.java
@@ -0,0 +1,19 @@
+package com.codewar.training;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IsogramsTest {
+    @Test
+    public void FixedTests() {
+        assertEquals(true, Isograms.isIsogram("Dermatoglyphics"));
+        assertEquals(true, Isograms.isIsogram("isogram"));
+        assertEquals(false, Isograms.isIsogram("moose"));
+        assertEquals(false, Isograms.isIsogram("isIsogram"));
+        assertEquals(false, Isograms.isIsogram("aba"));
+        assertEquals(false, Isograms.isIsogram("moOse"));
+        assertEquals(true, Isograms.isIsogram("thumbscrewjapingly"));
+        assertEquals(true, Isograms.isIsogram(""));
+    }
+}
