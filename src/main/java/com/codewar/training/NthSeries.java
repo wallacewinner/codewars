70e6e72157c96a82048e5270204c446009b77ee5
Wallace Winner <wallacewinner@hotmail.com>
2022-12-26 20:16:56 -0300

    feat: set solution to NthSeries

--- src/main/java/com/codewar/training/NthSeries.java
+++ src/main/java/com/codewar/training/NthSeries.java
@@ -1,7 +1,17 @@
 package com.codewar.training;
 
+import java.text.DecimalFormat;
+
 public class NthSeries {
+    static DecimalFormat df = new DecimalFormat("0.00");
+
     public static String seriesSum(int i) {
-        return null;
+        double result = 0;
+        var initial = 1;
+        for (var j = 1; j <= i; j++) {
+            result += (1/ (double) initial);
+            initial += 3;
+        }
+        return String.valueOf(df.format(result));
     }
 }
