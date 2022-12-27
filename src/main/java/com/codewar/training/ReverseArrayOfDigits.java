5a4b938a00945583c0b99f699889be65256cf433
Wallace Winner <wallacewinner@hotmail.com>
2022-12-26 23:46:55 -0300

    feat: set return when n is equals 0

--- src/main/java/com/codewar/training/ReverseArrayOfDigits.java
+++ src/main/java/com/codewar/training/ReverseArrayOfDigits.java
@@ -2,6 +2,12 @@ package com.codewar.training;
 
 public class ReverseArrayOfDigits {
     public static int[] digitize(long n) {
+        if (n == 0) {
+            return new int[0];
+        }
+
+
+
         return null;
     }
 }
