393bae383a0f65fbe71466aaa7f695809cbe2337
Wallace Winner <wallacewinner@hotmail.com>
2022-12-27 00:27:25 -0300

    feat: set a method to split de int and return a int[] array reversed

--- src/main/java/com/codewar/training/ReverseArrayOfDigits.java
+++ src/main/java/com/codewar/training/ReverseArrayOfDigits.java
@@ -1,13 +1,25 @@
 package com.codewar.training;
 
+import java.util.List;
+
 public class ReverseArrayOfDigits {
     public static int[] digitize(long n) {
-        if (n == 0) {
-            return new int[0];
+        if (n == 0) return new int[]{0};
+
+        long number = n;
+        List<Integer> digits = new java.util.ArrayList<Integer>();
+
+        while (number > 0) {
+            int digit = (int) (number % 10);
+            number /= 10;
+            digits.add(digit);
         }
 
+        int[] result = new int[digits.size()];
+        for (int i = 0; i < digits.size(); i++) {
+            result[i] = digits.get(i);
+        }
 
-
-        return null;
+        return result;
     }
 }
