f51f62c383dccc040ce2bb318a09768e45c34247
Wallace Winner <wallacewinner@hotmail.com>
2022-12-28 21:59:10 -0300

    test: init class

---
+++ src/main/java/com/codewar/training/WillYouMakeIt.java
@@ -0,0 +1,7 @@
+package com.codewar.training;
+
+public class WillYouMakeIt {
+    public static boolean zeroFuel(int distanceToPump, int mpg, int fuelLeft) {
+        return false;
+    }
+}
---
+++ src/test/java/com/codewar/training/WillYouMakeItTest.java
@@ -0,0 +1,13 @@
+package com.codewar.training;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+ class WillYouMakeItTest {
+
+    @Test
+     void testSomething() {
+        assertTrue(WillYouMakeIt.zeroFuel(50, 25, 2));
+        assertFalse(WillYouMakeIt.zeroFuel(100, 50, 1));
+    }
+}
