6df1200dfe6e6f061a5398208d26391aaac1cd66
Wallace Winner <wallacewinner@hotmail.com>
2022-12-28 22:10:14 -0300

    feat: set method to calc the fuel and return a boolean

--- src/main/java/com/codewar/training/WillYouMakeIt.java
+++ src/main/java/com/codewar/training/WillYouMakeIt.java
@@ -10,6 +10,6 @@ package com.codewar.training;
  */
 public class WillYouMakeIt {
     public static boolean zeroFuel(int distanceToPump, int mpg, int fuelLeft) {
-        return false;
+        return distanceToPump <= mpg * fuelLeft;
     }
 }
